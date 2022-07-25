8ac0174d3ccf23e4c99841a995048ffdf92a8861
Jennifer <jenniferfgriffin@utexas.edu>
2022-07-25 13:15:11 -0500

    server successfully gets all books

---
+++ controllers/index.js
@@ -0,0 +1,40 @@
+const Book = require('../models/book')
+const Author = require('../models/author')
+
+// Book CRUD
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find()
+    return res.status(200).json({ books })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const createBook = async (req, res) => {
+  try {
+    const book = await new Book(req.body)
+    await book.save()
+    return res.status(201).json({
+      book
+    })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+//Author CRUD
+const getAllAuthors = async (req, res) => {
+  try {
+    const authors = await Author.find()
+    return res.status(200).json({ plants })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+module.exports = {
+  createBook,
+  getAllAuthors,
+  getAllBooks
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const { Router } = require('express')
+const controllers = require('../controllers')
 const router = Router()
 
 router.get('/', (req, res) => res.send('This is root!'))
+router.post('/books', controllers.createBook)
+router.get('/books', controllers.getAllBooks)
+router.get('/author', controllers.getAllAuthors)
 
 module.exports = router
