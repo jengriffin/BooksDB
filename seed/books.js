3675faeafde1bbbaa029bf20d2f15cf7eda02640
Jennifer <jenniferfgriffin@utexas.edu>
2022-07-25 11:26:21 -0500

    books db working

--- seed/books.js
+++ seed/books.js
@@ -1,16 +1,18 @@
 const db = require('../db')
-const Book = require('../models/book)
+const Book = require('../models/book')
 
 // Connect to the database
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
   const books = [
-    name: 'Chicka Chicka Boom Boom',
-    description:
-      'This book is great for teaching kids about the alphabet. Its fun and engaging, and can also allow students to participate in the reading. The rhythmic pattern great for early readers.',
-    image:
-      'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781534457119/chicka-chicka-boom-boom-9781534457119_hr.jpg'
+    {
+      name: 'Chicka Chicka Boom Boom',
+      description:
+        'This book is great for teaching kids about the alphabet. Its fun and engaging, and can also allow students to participate in the reading. The rhythmic pattern great for early readers.',
+      image:
+        'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781534457119/chicka-chicka-boom-boom-9781534457119_hr.jpg'
+    }
   ]
 
   await Book.insertMany(books)
