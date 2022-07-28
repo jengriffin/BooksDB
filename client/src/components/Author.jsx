245d6f7f5c6eb1e13a89a80faac6d9723775dabc
Jennifer <jenniferfgriffin@utexas.edu>
2022-07-28 15:16:01 -0500

    Author component return add

--- client/src/components/Author.jsx
+++ client/src/components/Author.jsx
@@ -1,6 +1,14 @@
-const Author = () => {
+const Author = (props) => {
   return (
-    <button>Click Me</button>
+    <div className="card game-card">
+    <div className="img-wrapper">
+    <h3>{props.name}</h3>
+    </div>
+    <div className="info-wrapper flex-row space">
+      <h3>{props.books}</h3>
+      <h1>{props.bio}</h1>
+    </div>
+  </div>
   )
 }
 export default Author
